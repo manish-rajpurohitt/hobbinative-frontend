74d280428accd879294492d5a9930d121dc5485d
satyasuryavenkat@gmail.com <satyasuryavenkat@gmail.com>
2021-12-12 17:04:36 +0530

    push

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,62 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Select from 'react-select';
 import './index.css';
 
-const HomePage = () =>{
 
 
+
+
+
+
+const Dashboard = () =>{
+
+   const [values,setValues] = useState([]);
+   const [checked,setChecked] = useState(false);
+   const options = [
+      { label: "Option 1", value: 1 },
+      { label: "Option 2", value: 2 },
+      { label: "Option 3", value: 3 },
+      { label: "Option 4", value: 4 },
+      { label: "Option 5", value: 5 },
+      { label: "Option 6", value: 6 },
+      { label: "Option 7", value: 7 }
+    ];
+   const navigate = useNavigate();
+  
+   const user = JSON.parse(localStorage.getItem('user'));
+   const token = localStorage.getItem('token');
+   console.log(token);
+
+  const onChangeCheckbox = e => {
+      const isChecked = !this.state.checked;
+      setChecked(isChecked)
+        if(isChecked)
+        setValues(options)
+        else
+        setValues(values)
+       
+    
+    };
+   const onChange = opt => {
+      const allOptionsSelected = opt.length === options.length;
+      
+      if(allOptionsSelected)
+      setChecked(true);
+      else
+      setChecked(false);
+       setValues(opt)
+        
+      
+    };
+   
+
+   const handleLogout=()=>{
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      navigate('/')
+   };
+  
     return (
         <div className="home">
 
@@ -17,7 +69,23 @@ const HomePage = () =>{
                   <li><a href="#">DashBoard</a></li>
                 
                    <li><a href="#">Profile</a></li>
-                  <button className="nav-btn">
+                
+                     <div style={{
+                        marginTop:25,
+                        marginRight:10
+                     }}>
+                  <Select
+                  
+                  isMulti
+                  onChange={onChange}
+                  options={options}
+                  value={values}
+                  />
+                  </div>
+      
+         
+             
+                  <button className="nav-btn" onClick={handleLogout}>
                   <p className="btn-txt"> Logout</p>
                   </button>    
                </ul>
@@ -25,6 +93,8 @@ const HomePage = () =>{
          </header>
        
          <p className="breadcrum">Dashboard</p>
+
+       
          
          </div>
          
@@ -34,4 +104,4 @@ const HomePage = () =>{
     );
 }
 
-export default HomePage;
+export default Dashboard;
